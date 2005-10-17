78aacc71588516a113e7d0d41ffdc747920651a7
Samuel Rivas <samuel.rivas@lambdastream.com>
2005-10-17 16:36:07 +0000

    DCT and IDCT naive (i.e. slow) implementations
    git-svn-id: file:///mnt/svn-repos/samuel/Imolib/trunk@5 7c5df719-0403-0410-80dc-caeddea23c49

---
+++ imolib_dct.m
@@ -0,0 +1,42 @@
+## usage imolib_dct(M)
+
+function Transform = imolib_idct(M)
+
+  N = size(M, 1);
+  if (N != size(M, 2))
+    error("M must be square");
+  end
+
+  Transform = zeros(N,N);
+
+  for U = 0 : N - 1
+    for V = 0 : N - 1
+      Transform(U + 1, V + 1) = coefficient(U, V, M);
+    end
+  end
+
+endfunction
+
+function Coeff = coefficient(U, V, M)
+
+  N = length(M);
+
+  sum = 0;
+
+  for X = 0 : N - 1
+    for Y = 0 : N - 1
+      sum = sum + M(X + 1 , Y + 1) * cos((2*X + 1) * U * pi / (2*N)) \
+	  * cos((2*Y + 1) * V * pi / (2*N));
+    end
+  end
+
+  if (U == 0 && V == 0)
+    C = 1/2;
+  elseif (U == 0 || V == 0)
+    C = 1/sqrt(2);
+  else 
+    C = 1;
+  end
+
+  Coeff = 2 * C * sum / N;
+endfunction
---
+++ imolib_idct.m
@@ -0,0 +1,49 @@
+## usage imolib_dct(M)
+
+function M = imolib_dct(Transform)
+
+  N = size(Transform, 1);
+  if (N != size(Transform, 2))
+    error("Transform must be square");
+  end
+
+  M = zeros(N,N);
+
+  for X = 0 : N - 1
+    for Y = 0 : N - 1
+      M(X + 1, Y + 1) = value(X, Y, Transform);
+    end
+  end
+
+endfunction
+
+function Val = value(X, Y, Transform)
+
+  N = length(Transform);
+
+  sum = 0;
+
+  for U = 0 : N - 1
+
+    if (U == 0)
+      Cu = 1 / sqrt(2);
+    else
+      Cu = 1;
+    end
+
+    for V = 0 : N - 1
+      
+      if (V == 0)
+	Cv = 1 / sqrt(2);
+      else
+	Cv = 1;
+      end
+      
+      sum = sum + Cu * Cv * Transform(U + 1, V + 1) \
+	  * cos((2*X + 1) * U * pi/(2*N)) \
+	  * cos((2*Y + 1) * V * pi/(2*N));
+    end
+  end
+
+  Val = 2 * sum / N;
+endfunction
