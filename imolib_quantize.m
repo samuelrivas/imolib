6ea9e78978092d2534e3ff3e0a566fb8db1694c4
Samuel Rivas <samuel.rivas@lambdastream.com>
2005-10-20 17:04:07 +0000

    The inverse quantization has been moved to the imolib_quantize function
    git-svn-id: file:///mnt/svn-repos/samuel/Imolib/trunk@7 7c5df719-0403-0410-80dc-caeddea23c49

--- imolib_quantize.m
+++ imolib_quantize.m
@@ -1,24 +1,34 @@
-## Usage QM = imolib_quantize(M, Factor, QuantMatrix)
+## Usage QM = imolib_quantize(M, Factor, Inverse, QuantMatrix)
 ##
 ## Quantizes the M matrix with Factor. If QuantMatrix is present, an
-## additional factor is used for each individual coefficient
+## additional factor is used for each individual coefficient.
+##
+## The inverse parameter is also optional. If it is non-zero then
+## an inverse quantization is performed
 ##
 ## The operation is roughly the next:
 ## QM = round(M ./ (Factor * QuantMatrix))
 
-function QM = imolib_quantize(M, Factor, QuantMatrix)
+function QM = imolib_quantize(M, Factor, Inverse, QuantMatrix)
 
-  if (nargout > 1 || (nargin != 2 && nargin != 3))
-    usage(["QM = imolib_quantize(M, Factor, QuantMatrix)\nQuantMatrix ", \
-	   "is optional"]);
+  if (nargout > 1 || nargin < 2 || nargin  > 4)
+    usage("QM = imolib_quantize(M, Factor, Inverse, QuantMatrix)\n");
   end
 
-  if (nargin != 3)
+  if (nargin != 4)
     QuantMatrix = ones(size(M));
   elseif (~ all(size(M) == size(QuantMatrix)))
     error("QuantMatrix and M dimensions don't agree");
   end
 
-  QM = round(M ./ (Factor * QuantMatrix));
+  if (nargin == 2)
+    Inverse = 0;
+  end
+
+  if (Inverse)
+    QM = M .* QuantMatrix * Factor;
+  else 
+    QM = round(M ./ (Factor * QuantMatrix));
+  end
 
 endfunction
