3c964dec92a23a2a6efe96dccf97970035b50105
Samuel Rivas <samuel.rivas@lambdastream.com>
2005-10-27 18:01:58 +0000

    Added the fdct function
    git-svn-id: file:///mnt/svn-repos/samuel/Imolib/trunk@13 7c5df719-0403-0410-80dc-caeddea23c49

---
+++ imolib_fdct.m
@@ -0,0 +1,52 @@
+## Usage Transform imolib_fdct(M, Inverse)
+##
+## M must be an 8x8 matrix
+##
+
+##
+## A (not so) fast implementation of the 8x8 discrete cosine transform
+## Two possible improvements are:
+##
+## - Caching the C matrix to avoid recalculating it in each call
+## - Explote the C matrix preiodicity to save some computations
+##
+function Transform = imolib_fdct(M, Inverse)
+
+  ## Check input
+  if (nargout > 1 || nargin < 1 || nargin > 2)
+    usage("Transform = imolib_fdct(M, Inverse)");
+  end
+
+  if (!all(size(M) == [8,8]))
+    error("M must be 8x8");
+  end
+
+  if (nargin == 1)
+    Inverse = false;
+  end
+
+  if (!isbool(Inverse))
+    error("Inverse must be boolean");
+  end
+
+  C = zeros(8);
+
+  for X = 1:8
+
+    if (X == 1)
+      S = sqrt(0.125);
+    else 
+      S = 0.5;
+    end
+
+    for Y = 1:8
+      C(X,Y) = S * cos((pi/8)*(X - 1)*(Y-0.5));
+    end
+  end
+
+  if (!Inverse)
+    Transform = C*M*C';
+  else
+    Transform = C'*M*C;
+  end
+endfunction;
