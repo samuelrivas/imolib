8bc0b7265a4c92dc5b318d619c31f07b527c3458
Samuel Rivas <samuel.rivas@lambdastream.com>
2005-11-08 17:12:48 +0000

    A much faster implementation. It does not saturate nor round for now
    git-svn-id: file:///mnt/svn-repos/samuel/Imolib/trunk@21 7c5df719-0403-0410-80dc-caeddea23c49

--- imolib_yuv2rgb.m
+++ imolib_yuv2rgb.m
@@ -6,20 +6,16 @@ function [R, G, B] = imolib_yuv2rgb(Y, U, V, Reverse)
     Reverse = false;
   end
   
-  if (Reverse)
-    Coeff = ([0.299 0.587 0.114; -0.147 -0.289 0.436; 0.615 -0.515 -0.100]);
-  else 
-    Coeff = ([1 0 1.140; 1 -0.395 -0.58; 1 2.032 0]);
-  end
   
   R = G = B = zeros(size(Y));
 
-  for I = 1:size(Y)(1)
-    for J = 1:size(Y)(1)
-      Triplet = Coeff * [Y(I,J); U(I,J); V(I,J)];
-      R(I,J) = Triplet(1);
-      G(I,J) = Triplet(2);
-      B(I,J) = Triplet(3);
-    end
+  if (Reverse)
+    R = 0.257*Y + 0.504*U + 0.098*V + 16;
+    G = -0.148*Y - 0.291*U + 0.439*V + 128; 
+    B = 0.439*Y - 0.368*U - 0.071*V + 128;
+  else 
+    R = 1.164*(Y - 16) + 1.596*(V - 128);
+    G = 1.164*(Y - 16) - 0.813*(V - 128) - 0.391*(U - 128);
+    B = 1.164*(Y - 16) + 2.018*(U - 128);
   end
 endfunction
